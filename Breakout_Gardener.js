7501d1166e760ae3c19331a5410025d376d07af1
KHH <43569368+xoblite@users.noreply.github.com>
2019-02-05 21:06:55 +0100

    Version 19.2.5

---
+++ Breakout_Gardener.js
@@ -0,0 +1,323 @@
+// ============================================================================================
+// --- BREAKOUT GARDENER ---
+// "A nice little playground for miscellaneous (p)HAT/Breakout/Sensor/OLED tinkering" ;)
+// (c) 2018-2019 Karl-Henrik Henriksson - breakouts*xoblite.net - http://breakouts.xoblite.net/
+// ============================================================================================
+
+const softwareName = "Breakout Gardener";
+const softwareVersion = "19.2.5";
+
+const I2C = require('./modules/I2C.js'); // Requires the i2c-bus library -> https://github.com/fivdi/i2c-bus
+
+const ADS1015 = require('./modules/ADS1015.js');
+const BMP280 = require('./modules/BMP280.js');
+const CAP1166 = require('./modules/CAP1166.js');
+const DS18B20 = require('./modules/DS18B20.js');
+const IS31FL3731 = require('./modules/IS31FL3731.js');
+const LSM303D = require('./modules/LSM303D.js');
+const MCP9808 = require('./modules/MCP9808.js');
+const SH1107 = require('./modules/SH1107.js');
+const TCS3472 = require('./modules/TCS3472.js');
+const VEML6075 = require('./modules/VEML6075.js');
+
+const SYSTEM = require('./modules/System.js');
+const CLOCK = require('./modules/Clock.js');
+const KOMPIS = require('./modules/Kompis.js');
+const DASHBOARD = require('./modules/Dashboard.js');
+const PROMETHEUS = require('./modules/Prometheus.js');
+
+// ================================================================================
+// ========================= USER CONFIGURABLE SETTINGS ===========================
+// ================================================================================
+
+// General settings
+const outputLogs = true;
+const showDebug = true;
+
+// DASHBOARD related settings
+const dashboardEnabled = true; // Enable dashboard server?
+const dashboardPort = 8080; // Dashboard server port (when enabled)
+
+// PROMETHEUS related settings
+const prometheusEnabled = true; // Enable Prometheus exposure server?
+const prometheusPort = 9091; // Prometheus exposure server port (when enabled)
+
+// DS18B20 related settings
+// -> Identity of any DS18B20 1-Wire sensor device(s) we're interested in
+// (nb. each sensor has a unique 28-xxxxxxxxxxxx serial number ~ ID string,
+// for further information see e.g. https://pinout.xyz/pinout/1_wire )
+var ds18b20Sensors = ['28-01183392d2ff', '28-011833915dff'];
+
+// LSM303D related settings
+var lsm303dDisplayRaw = false; // Display motion data as raw sensor values instead of as scale adjusted g/gauss units?
+var lsm303dHeadingCalibration = 0; // Manual compass heading calibration (in case it's needed [?])
+
+// TCS3472 related settings
+var tcs3472LEDEnable = false; // Enable the TCS3472 illumination LEDs on the Enviro pHAT?
+
+// ================================================================================
+// ================= DISCOVER, START AND INITIALIZE ALL MODULES ===================
+// ================================================================================
+
+console.log("%s -> INFO -> Starting: Version %s, running on Node.js %s.", softwareName, softwareVersion, process.version);
+
+// Open the I2C bus and scan for devices...
+I2C.Start(outputLogs, showDebug);
+/*
+if (!SH1107.IsAvailable())
+{
+    // Since the OLED display breakout is (for now at least) quite essential to the functionality of
+    // this application, we will terminate if no supported display device was found on the I2C bus...
+    console.log("%s -> ERROR -> No supported display was found on I2C bus 1! Terminating...", softwareName);
+    I2C.Stop();
+    return;
+}
+*/
+// Start and initialize all supporting subsystems...
+SYSTEM.Start(outputLogs, showDebug);
+if (SH1107.IsAvailable()) SH1107.Start(outputLogs, showDebug);
+if (IS31FL3731.IsAvailable()) IS31FL3731.Start(outputLogs, showDebug);
+
+if (ADS1015.IsAvailable()) ADS1015.Start(outputLogs, showDebug);
+if (BMP280.IsAvailable()) BMP280.Start(outputLogs, showDebug);
+if (CAP1166.IsAvailable()) CAP1166.Start(outputLogs, showDebug);
+if (LSM303D.IsAvailable()) LSM303D.Start(lsm303dHeadingCalibration, lsm303dDisplayRaw, outputLogs, showDebug);
+if (MCP9808.IsAvailable()) MCP9808.Start(outputLogs, showDebug);
+if (TCS3472.IsAvailable()) TCS3472.Start(tcs3472LEDEnable, outputLogs, showDebug);
+if (VEML6075.IsAvailable()) VEML6075.Start(outputLogs, showDebug);
+
+DS18B20.Identify(ds18b20Sensors);
+if (DS18B20.IsAvailable()) DS18B20.Start(outputLogs, showDebug);
+
+if (dashboardEnabled) DASHBOARD.Start(dashboardPort, softwareVersion, outputLogs, showDebug);
+if (prometheusEnabled) PROMETHEUS.Start(prometheusPort, outputLogs, showDebug);
+
+// ================================================================================
+// ================ OLED ROTATING DISPLAY MODE AND RELATED FUNCTIONS ==============
+// ================================================================================
+
+// Definition of display modes... (nb. just assigning simple identities, enum-style)
+const DISPLAY_MODE_CPU_LOAD_BARS = 1;
+const DISPLAY_MODE_SYSTEM_LOAD = 2;
+const DISPLAY_MODE_24H_CLOCK = 3;
+const DISPLAY_MODE_KOMPIS_FRIEND = 4;
+const DISPLAY_MODE_ADS1015 = 5;
+const DISPLAY_MODE_BMP280 = 6;
+const DISPLAY_MODE_DS18B20 = 7;
+const DISPLAY_MODE_LSM303D = 8;
+const DISPLAY_MODE_MCP9808 = 9;
+const DISPLAY_MODE_TCS3472 = 10; 
+const DISPLAY_MODE_VEML6075 = 11;
+
+// The actual order of the display modes... (auto-rotating unless locked)
+const displayModes = [
+    DISPLAY_MODE_24H_CLOCK,
+    DISPLAY_MODE_CPU_LOAD_BARS,
+    DISPLAY_MODE_SYSTEM_LOAD,
+    DISPLAY_MODE_MCP9808,
+    DISPLAY_MODE_BMP280,
+    DISPLAY_MODE_DS18B20,
+    DISPLAY_MODE_TCS3472,
+    DISPLAY_MODE_LSM303D,
+    DISPLAY_MODE_ADS1015,
+    DISPLAY_MODE_VEML6075,
+    DISPLAY_MODE_KOMPIS_FRIEND
+];
+
+// Initialize various display mode related settings at startup...
+var currentDisplayMode = 0;
+var previousDisplayMode = 0xffff;
+var displayModeInterval = null;
+var displayModeAutoRotate = true;
+var displayModeFastRefresh = true;
+var displaysTurnedOff = false;
+
+// ====================
+
+function updateDisplayMode()
+{
+    clearInterval(displayModeInterval);
+
+    var refreshAll = true;
+    if (currentDisplayMode == previousDisplayMode) refreshAll = false;
+
+    switch (displayModes[currentDisplayMode])
+    {
+        case DISPLAY_MODE_CPU_LOAD_BARS:{ SYSTEM.DisplayLoad(refreshAll); break; }
+        case DISPLAY_MODE_SYSTEM_LOAD: { SYSTEM.DisplayInfo(refreshAll); break; }
+        case DISPLAY_MODE_24H_CLOCK: { CLOCK.Display(refreshAll); break; }
+        case DISPLAY_MODE_KOMPIS_FRIEND: { KOMPIS.Display(refreshAll); break; }
+        case DISPLAY_MODE_ADS1015: { ADS1015.Display(refreshAll); break; }
+        case DISPLAY_MODE_BMP280: { BMP280.Display(refreshAll); break; }
+        case DISPLAY_MODE_DS18B20: { DS18B20.Display(refreshAll); break; }
+        case DISPLAY_MODE_LSM303D: { LSM303D.Display(refreshAll); break; }
+        case DISPLAY_MODE_MCP9808: { MCP9808.Display(refreshAll); break; }
+        case DISPLAY_MODE_TCS3472: { TCS3472.Display(refreshAll); break; }
+        case DISPLAY_MODE_VEML6075: { VEML6075.Display(refreshAll); break; }
+        default: { break; }
+    }
+
+    previousDisplayMode = currentDisplayMode;
+
+    if (displayModeAutoRotate) // Display mode auto-rotating
+    {
+        currentDisplayMode++;
+        if (currentDisplayMode == displayModes.length) currentDisplayMode = 0;
+        displayModeInterval = setInterval(updateDisplayMode, 5000); // Display mode rotating -> Lower refresh rate
+    }
+    else // Display mode locked
+    {
+        if (displayModes[currentDisplayMode] == DISPLAY_MODE_KOMPIS_FRIEND) displayModeInterval = setInterval(updateDisplayMode, 250); // Animation @ 4 FPS! :)
+        else if (displayModeFastRefresh) displayModeInterval = setInterval(updateDisplayMode, 1000); // -> Higher refresh rate, updating every second
+        else displayModeInterval = setInterval(updateDisplayMode, 10000); // -> Lower refresh rate, updating every 30 seconds
+    }
+}
+
+updateDisplayMode();
+
+// ================================================================================
+// =================== TOUCH PHAT / INPUT RELATED FUNCTIONALITY ===================
+// ================================================================================
+
+function updateTouchLEDs(current) // Helper function to pollTouchButtons()
+{
+    var leds = current;
+    if (displaysTurnedOff) leds |= 0b010000;
+    else leds &= 0b101111;
+    if (displayModeAutoRotate) leds |= 0b001000;
+    else leds &= 0b110111;
+    if (displayModeFastRefresh) leds |= 0b000100;
+    else leds &= 0b111011;
+    CAP1166.SetLEDs(leds);
+}
+
+function pollTouchButtons() // Periodic touch polling function (see setInterval() call below)
+{
+    var data = CAP1166.Get();
+
+    if (data[0] > 0) // ===== Touch detected! =====
+    {
+        if (data[0] & 32) // Button "<" -> Previous display mode
+        {
+            if (!displayModeAutoRotate)
+            {
+                clearInterval(displayModeInterval);
+                if (currentDisplayMode == 0) currentDisplayMode = (displayModes.length - 1);
+                else currentDisplayMode--;
+                previousDisplayMode = 0xffff;
+            }
+        }
+
+        if (data[0] & 1) // Button ">" -> Next display mode
+        {
+            if (!displayModeAutoRotate)
+            {
+                clearInterval(displayModeInterval);
+                currentDisplayMode++;
+                if (currentDisplayMode == displayModes.length) currentDisplayMode = 0;
+                previousDisplayMode = 0xffff;
+            }
+        }
+
+        // ====================
+
+        if (data[0] & 16) // Button "A" -> Turn off/on displays
+        {
+            if (displaysTurnedOff)
+            {
+                displaysTurnedOff = false;
+                previousDisplayMode = 0xffff;
+                SH1107.On();
+                IS31FL3731.On();
+            }
+            else
+            {
+                clearInterval(displayModeInterval);
+                displaysTurnedOff = true;
+                SH1107.Off();
+                IS31FL3731.Off();
+            }
+        }
+
+        if (data[0] & 8) // Button "B" -> Lock the current display mode (i.e. disable auto-rotate)
+        {
+            clearInterval(displayModeInterval);
+            if (displayModeAutoRotate)
+            {
+                displayModeAutoRotate = false;
+                if (currentDisplayMode == 0) currentDisplayMode = displayModes.length - 1;
+                else currentDisplayMode--; // We need to do this as updateDisplayMode() increases on every loop in preparation for the next loop...
+                previousDisplayMode = currentDisplayMode;
+            }
+            else displayModeAutoRotate = true;
+        }
+
+        if (data[0] & 4) // Button "C" -> Toggle fast/slow update frequency for locked mode
+        {
+            clearInterval(displayModeInterval);
+            if (displayModeFastRefresh) displayModeFastRefresh = false;
+            else displayModeFastRefresh = true;
+        }
+/*
+        if (data[0] & 2) // Button "D" -> Currently unassigned...
+        {
+            clearInterval(displayModeInterval);
+        }
+*/
+        // ====================
+
+        // Update the LEDs if a button was pressed... (otherwise nothing has changed)
+        if (data[0] > 0b00000000) updateTouchLEDs(data[1]);
+        // And finally, update the display mode...
+        if (!displaysTurnedOff) updateDisplayMode();
+    }
+}
+
+// ====================
+
+var touchPollingInterval = null;
+if (CAP1166.IsAvailable())
+{
+    updateTouchLEDs(0b000000); // Update LEDs to initial state
+    touchPollingInterval = setInterval(pollTouchButtons, 250); // Start touch polling, 250 msec interval
+}
+
+// ================================================================================
+// ===================== STOP ALL MODULES & CLEAN UP ON EXIT ======================
+// ================================================================================
+
+function StopAll()
+{
+    console.log("%s -> INFO -> Terminating: Stopping all modules and cleaning up...", softwareName);
+    
+    clearInterval(touchPollingInterval);
+    clearInterval(displayModeInterval);
+
+    // Let's stop the modules in the opposite order
+    // of how we started them, just in case... ;)
+    if (prometheusEnabled) PROMETHEUS.Stop();
+    if (dashboardEnabled) DASHBOARD.Stop();
+    if (DS18B20.IsAvailable()) DS18B20.Stop();
+
+    if (VEML6075.IsAvailable()) VEML6075.Stop();
+    if (TCS3472.IsAvailable()) TCS3472.Stop();
+    if (MCP9808.IsAvailable()) MCP9808.Stop();
+    if (LSM303D.IsAvailable()) LSM303D.Stop();
+    if (CAP1166.IsAvailable()) CAP1166.Stop();
+    if (BMP280.IsAvailable()) BMP280.Stop();
+    if (ADS1015.IsAvailable()) ADS1015.Stop();
+
+    if (IS31FL3731.IsAvailable()) IS31FL3731.Stop();
+    if (SH1107.IsAvailable()) SH1107.Stop();
+    SYSTEM.Stop();
+	I2C.Stop();
+}
+
+process.on('SIGINT', (code) => {
+    StopAll();
+});
+process.on('SIGTERM', (code) => {
+    StopAll();
+});
+
+// ================================================================================
